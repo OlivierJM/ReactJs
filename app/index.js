61c3bf9b662c9880ef58ae2659efa2dce841202a
OlivierJM <manolivier93@gmail.com>
2016-11-03 17:08:05 +0200

    Added react-router with the firstPage

--- app/index.js
+++ app/index.js
@@ -1,16 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes.js')
+var Raven = require('raven-js')
 
-var HelloWorld = React.createClass({
-  render: function () {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
-  }
-})
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('app')
-)
+var sentryKey = '7c640567d546499fbad00ea1f5cc254'
+var sentryApp = '111715'
+var sentryURL = 'https://'+ sentryKey + 'a@sentry.io/' + sentryApp
+
+
+Raven.config(sentryURL).install()
+
+ReactDOM.render(routes, document.getElementById('app'));
